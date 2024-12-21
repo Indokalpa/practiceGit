749b6b493e4d6709fd94f5dcea261c99d287808f
indokalpa <indokalpasaikia1@gmail.com>
2024-12-21 17:51:06 +0530

    utilities.js added

---
+++ utilities.js
@@ -0,0 +1,9 @@
+function add(num1,num1){
+    var sum = num1 + num2;
+    console.log("The sum will be:" + sum);
+}
+
+function product(num1,num2){
+    var product = num1 * num2;
+    console.log("Product will be:" + product);
+}
